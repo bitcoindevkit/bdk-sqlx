8b11a89715588a5ac5cc72123c70902e38c74c27
matthiasdebernardini <m.f.debern@protonmail.com>
2025-04-21 14:44:00 -0500

    fixup: adding new sql file

--- migrations/postgres/01_bdk_wallet.sql
+++ migrations/postgres/01_bdk_wallet.sql
@@ -1,10 +1,13 @@
+-- Create the bdk_wallet schema
+CREATE SCHEMA IF NOT EXISTS "bdk_wallet";
+
 -- Schema version control
-CREATE TABLE IF NOT EXISTS version (
+CREATE TABLE IF NOT EXISTS "bdk_wallet"."version" (
     version INTEGER PRIMARY KEY
 );
 
 -- Network is the valid network for all other table data
-CREATE TABLE IF NOT EXISTS network (
+CREATE TABLE IF NOT EXISTS "bdk_wallet"."network" (
     wallet_name TEXT PRIMARY KEY,
     name TEXT NOT NULL
 );
@@ -13,30 +16,29 @@ CREATE TABLE IF NOT EXISTS network (
 -- descriptor is the complete descriptor string,
 -- descriptor_id is a sha256::Hash id of the descriptor string w/o the checksum,
 -- last revealed index is a u32
-CREATE TABLE IF NOT EXISTS keychain (
+CREATE TABLE IF NOT EXISTS "bdk_wallet"."keychain" (
     wallet_name TEXT NOT NULL,
     keychainkind TEXT NOT NULL,
     descriptor TEXT NOT NULL,
     descriptor_id BYTEA NOT NULL,
     last_revealed INTEGER DEFAULT 0,
     PRIMARY KEY (wallet_name, keychainkind)
-
 );
 
 -- Hash is block hash hex string,
 -- Block height is a u32
-CREATE TABLE IF NOT EXISTS block (
+CREATE TABLE IF NOT EXISTS "bdk_wallet"."block" (
     wallet_name TEXT NOT NULL,
     hash TEXT NOT NULL,
     height INTEGER NOT NULL,
     PRIMARY KEY (wallet_name, hash)
 );
-CREATE INDEX idx_block_height ON block (height);
+CREATE INDEX IF NOT EXISTS idx_block_height ON "bdk_wallet"."block" (height);
 
 -- Txid is transaction hash hex string (reversed)
 -- Whole_tx is a consensus encoded transaction,
 -- Last seen is a u64 unix epoch seconds
-CREATE TABLE IF NOT EXISTS tx (
+CREATE TABLE IF NOT EXISTS "bdk_wallet"."tx" (
     wallet_name TEXT NOT NULL,
     txid TEXT NOT NULL,
     whole_tx BYTEA,
@@ -48,7 +50,7 @@ CREATE TABLE IF NOT EXISTS tx (
 -- Outpoint vout
 -- TxOut value as SATs
 -- TxOut script consensus encoded
-CREATE TABLE IF NOT EXISTS txout (
+CREATE TABLE IF NOT EXISTS "bdk_wallet"."txout" (
     wallet_name TEXT NOT NULL,
     txid TEXT NOT NULL,
     vout INTEGER NOT NULL,
@@ -61,13 +63,13 @@ CREATE TABLE IF NOT EXISTS txout (
 -- Block hash hex string
 -- Anchor is a json serialized Anchor structure as JSONB,
 -- Txid is transaction hash hex string (reversed)
-CREATE TABLE IF NOT EXISTS anchor_tx (
+CREATE TABLE IF NOT EXISTS "bdk_wallet"."anchor_tx" (
     wallet_name TEXT NOT NULL,
     block_hash TEXT NOT NULL,
     anchor JSONB NOT NULL,
     txid TEXT NOT NULL,
     PRIMARY KEY (wallet_name, block_hash, txid),
-    FOREIGN KEY (wallet_name, block_hash) REFERENCES block(wallet_name, hash),
-    FOREIGN KEY (wallet_name, txid) REFERENCES tx(wallet_name, txid)
+    FOREIGN KEY (wallet_name, block_hash) REFERENCES "bdk_wallet"."block"(wallet_name, hash),
+    FOREIGN KEY (wallet_name, txid) REFERENCES "bdk_wallet"."tx"(wallet_name, txid)
 );
-CREATE INDEX idx_anchor_tx_txid ON anchor_tx (txid);
+CREATE INDEX IF NOT EXISTS idx_anchor_tx_txid ON "bdk_wallet"."anchor_tx" (txid);
